b388d2f1b9767fba6c81ae36d330afc6f751c128
0xATHERIS <ath3ris@proton.me>
2023-06-07 00:16:43 +0100

    feat: :sparkles:  - Added vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+      <Analytics />
+    </>
   )
 }
